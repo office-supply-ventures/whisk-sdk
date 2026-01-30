1971513fa316e4b0fe05e2283ac2346973f57f18
spennyp <hello@spencerperkins.me>
2026-01-29 18:55:13 -0800

    chore(steakhouse): cleanup metadata exports

--- packages/steakhouse/src/metadata/index.ts
+++ packages/steakhouse/src/metadata/index.ts
@@ -1,5 +1,2 @@
 export { STEAKHOUSE_VAULTS } from "./generated/vaults.js"
-
-export type { Address, VaultConfig, VaultProtocol, VaultType } from "./types.js"
-
-export { VAULT_TYPES } from "./types.js"
+export type { VaultConfig } from "./types.js"
