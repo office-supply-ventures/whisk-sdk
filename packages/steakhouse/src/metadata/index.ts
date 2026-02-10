7f7b4e9a5f54ebf0a7b732b0b37c8d5c0b1fa32d
spennyp <hello@spencerperkins.me>
2026-02-09 17:59:23 -0800

    feat(steakhouse): add chain addresses for steakhouse box factory

--- packages/steakhouse/src/metadata/index.ts
+++ packages/steakhouse/src/metadata/index.ts
@@ -1,2 +1,3 @@
+export { type ChainAddresses, getChainAddresses } from "./address-registry.js"
 export { STEAKHOUSE_VAULTS } from "./generated/vaults.js"
 export type { VaultConfig } from "./types.js"
